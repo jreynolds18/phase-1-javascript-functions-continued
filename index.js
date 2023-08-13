a1674e31fa1d5da6499f429ed63492b697540784
Jesse Reynolds <jessereynolds18@gmail.com>
2023-08-12 21:12:00 -0500

    work with functions within functions and IIFE

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(task = "roller-skate") {
+    return `This Saturday, I want to ${task}!`;
+}
+
+const mondayWork = function(task = "go to the office") {
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(compliment = "special") {
+        return `You are ${flair}${compliment}${flair}!`
+    }
+}
+const encouragement = wrapAdjective("~");
